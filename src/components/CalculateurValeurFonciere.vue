a187ffd9b8d1924f608837f043e4f58ec5b30b62
Paloma Tejeda <palomatejeda81@gmail.com>
2025-09-01 18:09:02 +0200

    fix hydration mismatch on inscription

--- src/components/CalculateurValeurFonciere.vue
+++ src/components/CalculateurValeurFonciere.vue
@@ -151,6 +151,14 @@ watch(
           size="0.75rem"
           :color="colors['error-color']"
       /></label>
+      <FormElementsInputField
+        id="hello"
+        name="hello"
+        label="hello"
+        type="text"
+        :required="true"
+        placeholder="Commencez à taper une adresse..."
+      />
       <ClientOnly>
         <FormElementsLocationForm @address="updateAddress" />
       </ClientOnly>
