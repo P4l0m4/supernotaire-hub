dfc33e13a8552b55fbe3b748112a4fa3229944c1
Paloma Tejeda <palomatejeda81@gmail.com>
2026-02-25 19:22:22 +0100

    Disable PostHog initialization in dev

--- src/plugins/posthog.client.ts
+++ src/plugins/posthog.client.ts
@@ -2,6 +2,23 @@ import { defineNuxtPlugin } from "#app";
 import posthog from "posthog-js";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  if (import.meta.dev) {
+    const noop = () => {};
+    const noopClient = {
+      capture: noop,
+      identify: noop,
+      reset: noop,
+      opt_out_capturing: noop,
+      opt_in_capturing: noop,
+    };
+
+    return {
+      provide: {
+        posthog: () => noopClient,
+      },
+    };
+  }
+
   const runtimeConfig = useRuntimeConfig();
   const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
     api_host: runtimeConfig.public.posthogHost,
